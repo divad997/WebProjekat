e8c1edd9580bcaec782d5db4d6d4331f4258ade2
Gaap97 <aleksandar.petrov.97@gmail.com>
2020-04-12 18:50:07 +0200

    Proba routinga ka profilu

--- ProjekatAgencija/src/app/app.module.ts
+++ ProjekatAgencija/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
