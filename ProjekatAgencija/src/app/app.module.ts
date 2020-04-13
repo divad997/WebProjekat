9394f23ef3aad6e1cf3854c4d49be3e2381e7682
Jovanovski <jovanovski.david@yahoo.com>
2020-04-13 18:06:47 +0200

    Dodate login, register stranice i uradjen redizajn aplikacije

--- ProjekatAgencija/src/app/app.module.ts
+++ ProjekatAgencija/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
